09f5283d2437daf18c9c7d12e3b13326a8c3ad66
kingma-sbw <kingma@sbw-media.ch>
2022-03-20 16:28:58 +0100

    Add viewport in header

--- index.php
+++ index.php
@@ -51,6 +51,7 @@ if( array_key_exists('url', $_GET) && (false===strpos($_GET['url'], base_url)) )
   <head>
 		<title>QR maker</title>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="lib/qrcode.js" defer></script>
     <style>
       :root {
