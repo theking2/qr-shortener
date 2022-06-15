4a20cde245ff09be3a70115ef1335eaa05899771
kingma-sbw <kingma@sbw-media.ch>
2022-06-15 08:48:19 +0200

    Remove link on qR Fixes #8

--- assets/main.js
+++ assets/main.js
@@ -1,20 +1,31 @@
 "use strict";
+/**
+ * returns a static (not live) NodeList representing a list of the document's elements that match the specified group of selectors. 
+ * @param {string} selector 
+ * @param {Element} [document] base 
+ * @returns {(NodeList|Element|null)}
+ */
+const $ = (selector, base = document) => {
+	let elements = base.querySelectorAll(selector);
+	return (elements.length == 1) ? elements[0] : elements;
+}
 
 document.addEventListener("DOMContentLoaded", ev => {
+  getSettings();
 
   // initial qr
   doQR(ev);
 
   // set the handler for the ok button
-  document.getElementById("do-qr").onclick = doQR;
-  document.getElementById("container").onclick = downloadSVG;
-  document.getElementById("bg-color").onchange = doQR;
-  document.getElementById("color").onchange = doQR;
-  document.getElementById("size").onchange = doQR;
+  $("#do-qr").onclick = doQR;
+  $("#container").onclick = downloadSVG;
+  $("#bg-color").onchange = doQR;
+  $("#color").onchange = doQR;
+  $("#size").onchange = doQR;
 
 
   // select all on focus
-  const url = document.getElementById('url');
+  const url = $('#url');
   url.addEventListener('focus', ev => ev.target.select());
 
   // clear query_string
@@ -29,7 +40,7 @@ document.addEventListener("DOMContentLoaded", ev => {
  */
 const doShorten = ev => {
 
-  const url = document.getElementById('url').value;
+  const url = $('#url').value;
   window.location.assign = `<?=base_url?>/?url=${url}`;
   window.location.reload();
 
@@ -39,21 +50,22 @@ const doShorten = ev => {
  * create a qr code of the contents of url
  */
 const doQR = ev => {
-  const url = document.getElementById('url').value;
+  const url = $('#url').value;
 
   if (url.length === 0) return; // no url, stop
 
   let qrcode = new QRCode({
     content: url,
     padding: 2,
-    width: document.getElementById('size').value, height: document.getElementById('size').value,
+    width: $('#size').value, height: $('#size').value,
     join: false,
-    color: document.getElementById('color').value,
-    background: document.getElementById('bg-color').value,
+    color: $('#color').value,
+    background: $('#bg-color').value,
     ecl: "L"
   });
 
-  document.getElementById("container").innerHTML = qrcode.svg();
+  $("#container").innerHTML = qrcode.svg();
+  saveSettings();
   ev.preventDefault();
 };
 
@@ -61,11 +73,11 @@ const doQR = ev => {
  * create a download link
  */
 const downloadSVG = () => {
-  const svg = document.getElementById('container').outerHTML;
+  const svg = $('#container').outerHTML;
   const blob = new Blob([svg.toString()]);
   const element = document.createElement("a");
   try {
-    const encode = new URL('', document.getElementById('shorten').dataset.fullUrl);
+    const encode = new URL('', $('#shorten').dataset.fullUrl);
     element.download = encode.hostname.replaceAll('.', '_') + ".svg";
   } catch {
     // probably don't have a url
@@ -75,4 +87,31 @@ const downloadSVG = () => {
   element.href = window.URL.createObjectURL(blob);
   element.click();
   element.remove();
-}
+};
+
+/**
+ * Read the settings from localStorage
+ */
+const getSettings = _ => {
+  if( window.localStorage.getItem('settings') ) {
+    const settings = JSON.parse(window.localStorage.getItem('settings'));
+    $('#bg-color').value = settings.bgColor;
+    $('#color').value = settings.color;
+    $('#size').value = settings.size;
+  } else {
+    $('#bg-color').value = '#ffffff';
+    $('#color').value = '#000000';
+    $('#size').value = '50';
+  }
+};
+/**
+ * Save the settings to localStorage
+ */
+const saveSettings = _ => {
+  const settings = {
+    bgColor: $('#bg-color').value,
+    color: $('#color').value,
+    size: $('#size').value
+  }
+  window.localStorage.setItem('settings', JSON.stringify(settings));
+};
